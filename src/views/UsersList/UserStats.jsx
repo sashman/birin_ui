f02e83a784da220c0f9e2bae2a953a703cde79cd
sashman <sashman90@gmail.com>
2019-06-18 22:35:09 +0100

    Removing unused imports

--- src/views/UsersList/UserStats.jsx
+++ src/views/UsersList/UserStats.jsx
@@ -16,8 +16,6 @@ import CardBody from "components/Card/CardBody.jsx";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
-import Button from "@material-ui/core/Button";
-import Icon from "@material-ui/core/Icon";
 
 moment.locale("en-gb");
 
